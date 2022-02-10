9299d04e5538bc20acc5c05237d47edf0288b8b4
leesiaacct <91469460+leesiaacct@users.noreply.github.com>
2022-02-11 05:31:37 +0900

    Update main.py
    슬랙봇 출력까지 작업한 파일입니다. 슬랙봇 출력은 테스트했을 때 문제 없이 작동하는 걸 확인했습니다.
    지금이 2월이라 일정이 없어 프로그램을 실행시키면 이 2개의 메시지가 뜹니다.
    '지정된 파일을 찾을 수 없습니다. / 시스템에 부착된 장치가 작동하지 않습니다.'
    일정이 없을 때 crawler에서 반환할 값을 따로 설정을 해줘야 오류가 해결될 것 같습니다!

--- eclass_reminder/main.py
+++ eclass_reminder/main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from datetime import datetime
 from crawler import get_4d_schedule
+import requests
 
 
 # r 뒤에 크롬드라이버 경로 입력
@@ -21,6 +22,21 @@ driver.implicitly_wait(2)
 agenda_btn = driver.find_element(By.ID, 'agenda')
 agenda_btn.click()
 
+#슬랙봇으로 메시지 보내기(slack_sdk는 현재 사용할 수 없어 함수 이용)
+def post_message(token, channel, text):
+    response = requests.post("https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer "+token},
+        data={"channel": channel,"text": text}
+    )
+    print(response)
+
+'''
+1. 슬랙봇 생성(https://dsbook.tistory.com/290 등 참고)
+2. 만든 앱 이용할 채널에 추가(채널 진입 -> 오른쪽 위 클릭 -> 통합 -> 앱 -> 앱 추가)
+'''
+ 
+#슬랙봇 토큰 정보 입력
+myToken = "xoxb-****"
 
 # 크롤링 정보(오늘~향후 3일 스케줄) 받아와서 챗봇 메세지 생성
 def main():
@@ -34,6 +50,7 @@ def main():
 				month, day = event_list[0]['ddline'].month, event_list[0]['ddline'].day
 				mssg_brief = f"{month}월 {day}일까지 마감인 과제: {len(event_list)}개"
 				# 메세지 출력
+				post_message(myToken, "#채널명", mssg_brief)
 				
 				for event in event_list:
 						time_left = event['ddline'] - today
@@ -42,3 +59,4 @@ def main():
             #챗봇 메세지2_마감시간 알림 용
 						mssg_detail = f"{event['course_title']}: {event['event_title']}의 마감까지 {days_left}일 {hours_left}시간 남음."
 						# 메세지 출력
+						post_message(myToken, "#채널명", mssg_detail)
